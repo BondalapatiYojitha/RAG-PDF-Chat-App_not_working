5a4b7ed1e22e8bc11973f7d0d81eeee80bd2a383
BondalapatiYojitha <167112766+BondalapatiYojitha@users.noreply.github.com>
2025-03-06 22:12:25 -0500

    Update admin.py
    Signed-off-by: BondalapatiYojitha <167112766+BondalapatiYojitha@users.noreply.github.com>

--- Admin/admin.py
+++ Admin/admin.py
@@ -15,9 +15,10 @@ aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 aws_region = os.getenv("AWS_REGION", "us-east-1")
 
-st.write("aws_access_key  : {aws_access_key}")
-st.write("aws_secret_key  : {aws_secret_key}")
-st.write("aws_region  : {aws_region}")
+st.write(f"aws_access_key  : {aws_access_key if aws_access_key else 'MISSING'}")
+st.write(f"aws_secret_key  : {aws_secret_key if aws_secret_key else 'MISSING'}")
+st.write(f"aws_region  : {aws_region if aws_region else 'MISSING'}")
+
 
 # Initialize Bedrock Client with credentials
 bedrock_client = boto3.client(
